3b539c095d6492351e5509032189465b5a914fd9
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-08-03 10:06:45 +0700

    Create troll.lua

---
+++ troll.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aidez/decaying_winter/main/GOODWILL_COMMAND_SCRIPT'),true))()
+wait(5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/IrishBaker/script/main/effects.lua'),true))()
