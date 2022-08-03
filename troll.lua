cecf6a111f042f3d77b842ff0ac9918b410f6ce3
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-08-03 10:18:59 +0700

    Update troll.lua

--- troll.lua
+++ troll.lua
@@ -1,6 +1,5 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aidez/decaying_winter/main/GOODWILL_COMMAND_SCRIPT'),true))()
-wait(5)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/IrishBaker/script/main/effects.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/IrishBaker/script/main/effects.lua')()
 local a = game:GetService("Lighting")
 local b = game:GetService("Lighting").Sky
 b:destroy()
