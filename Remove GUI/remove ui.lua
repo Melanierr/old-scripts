9c2e55935dc51c773d4ae16988e9ea44522688f1
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-08-12 18:33:46 +0700

    Create remove ui.lua

---
+++ Remove GUI/remove ui.lua
@@ -0,0 +1,27 @@
+
+--// variables
+local topbar = game:GetService("Players").LocalPlayer.PlayerGui.topbarkek.frame
+local blackscreens = game:GetService("Players").LocalPlayer.PlayerGui.fadetoblack.frame
+local miscui = game:GetService("ReplicatedStorage").misc.otherMenu
+local gasmask = game:GetService("ReplicatedStorage").misc.gasmaskoverlay
+local sponsor = game:GetService("Players").LocalPlayer.PlayerGui.mainHUD.SponsorFrame
+local spectate = game:GetService("Players").LocalPlayer.PlayerGui.endgamegui
+local troll = game:GetService("Players").LocalPlayer.PlayerGui.mainHUD.HurtOverlayA
+local trolls = game:GetService("Players").LocalPlayer.PlayerGui.mainHUD.HurtOverlayB
+
+
+--// stuff
+topbar.Visible = false
+blackscreens.Visible = false
+miscui.Visible = false
+sponsor.Visible = false
+
+--// Amogus mode
+-- troll:destroy()
+-- trolls:destroy()
+
+--// goggle stuff
+--local goggleui = game:GetService("Players").LocalPlayer.PlayerGui.goggles.goggle
+--local goggleui2 = game:GetService("ReplicatedStorage").misc.goggles.goggle
+--goggleui:destroy()
+--goggleui2:destroy()
