b61e97685cd03dec2a51242ad74f95b3cf7f56dd
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-08-13 15:05:09 +0700

    Update Remove Traps.lua

--- decaying winter/Remove Traps.lua
+++ decaying winter/Remove Traps.lua
@@ -4,9 +4,4 @@ local traps = game:GetService("Workspace").Interactables.ScavTraps
 traps:destroy()
 wait(10)
   end
-wait(1)
-game.StarterGui:SetCore("SendNotification", {
-    Text = "Traps are removed",
-    Icon = "rbxassetid://9327507252",
-    Duration = 4
-})
+
