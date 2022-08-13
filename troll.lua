144fe82216adb23fd3258ddd8d32b921aa60b52d
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-08-13 14:31:16 +0700

    Delete troll.lua

--- troll.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aidez/decaying_winter/main/GOODWILL_COMMAND_SCRIPT'),true))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/IrishBaker/script/main/effects.lua'),true))()
