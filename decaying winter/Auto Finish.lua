f9c8dcc995be961e6da2c23da140ca786c979a96
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-09-17 16:11:44 +0700

    Optimizes script for better utilizing

--- decaying winter/Auto Finish.lua
+++ decaying winter/Auto Finish.lua
@@ -1,53 +1,28 @@
-local args = {
-    [1] = workspace.InteractablesNoDel.Touch,
-    [2] = "touchthing",
-    [3] = 30515
-}
-
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args)) -- knife
-
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.Interactables.NoDel.Touch, "touchthing", 30515) -- knife
 wait(0.3)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-74, 22, 166)) -- goto cultist small bunker wait
 wait(2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to spawn
 wait(2)
-local args = {
-    [1] = workspace.InteractablesNoDel.Keycard, -- get keycard
-    [2] = "keycardget",
-    [3] = 30515
-}
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args))
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Keycard, "keycardget", 30515)
 wait(2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to spawn
 wait(4)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(72, -3, -198)) -- go to the basement door
-local args = {
-    [1] = workspace.InteractablesNoDel.Unlock, -- unloock the basement door
-    [2] = "unlockthing",
-    [3] = 30515
-}
-
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args))
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Unlock, "unlockthing", 30515)
 wait(3)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-14, -64, -250)) -- go to the small door
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-165, -77, -210)) -- go to the laptop room
 wait(0.2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(72, -3, -198)) -- go to the basement door
 wait(1)
-local args = {
-    [1] = workspace.InteractablesNoDel.Activate, -- activate laptop
-    [2] = "activatething",
-    [3] = 30515
-}
-
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args))
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Activate, "activatething", 30515)
 wait(1)
 local args = {
     [1] = workspace.InteractablesNoDel:FindFirstChild("Acquire Masks"), -- get mask
     [2] = "acquirething",
     [3] = 30515
 }
-
 game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args))
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to base
 wait(1)
