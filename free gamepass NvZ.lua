98b980c3a8ff4f4af007455b3a4ff016619e27fb
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-10-28 19:04:19 +0700

    Update free gamepass NvZ.lua

--- free gamepass NvZ.lua
+++ free gamepass NvZ.lua
@@ -3,7 +3,6 @@ for __,v in pairs(game.Players.LocalPlayer.ClassConfigurations.LocalGamepasses:G
 end
 for _, a in pairs(game.Players.LocalPlayer.ClassConfigurations.LocalBadges:GetChildren()) do
     a.HasBadge.Value = true
-    print(a)
 end
 game:GetService("ReplicatedStorage").Miscs.Events.GetCurrentStatsEvent:InvokeServer()
 game:GetService("ReplicatedStorage").Miscs.Events.GameplayUpdateEvent:FireServer()
