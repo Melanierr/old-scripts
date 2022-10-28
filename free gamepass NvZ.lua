5fc8d51a6a9a07885160411248698a953a135088
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-10-28 19:00:07 +0700

    Add files via upload

---
+++ free gamepass NvZ.lua
@@ -0,0 +1,9 @@
+for __,v in pairs(game.Players.LocalPlayer.ClassConfigurations.LocalGamepasses:GetChildren()) do
+        v.HasGamepass.Value = true
+end
+for _, a in pairs(game.Players.LocalPlayer.ClassConfigurations.LocalBadges:GetChildren()) do
+    a.HasBadge.Value = true
+    print(a)
+end
+game:GetService("ReplicatedStorage").Miscs.Events.GetCurrentStatsEvent:InvokeServer()
+game:GetService("ReplicatedStorage").Miscs.Events.GameplayUpdateEvent:FireServer()
