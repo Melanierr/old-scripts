87b68b0ac0561bee1ad850d7a12223407e5a725a
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-11-26 12:25:52 +0700

    Update Remove UI.lua

--- decaying winter/Remove UI.lua
+++ decaying winter/Remove UI.lua
@@ -1,12 +1,8 @@
-
---// variables
 local topbar = game:GetService("Players").LocalPlayer.PlayerGui.topbarkek.frame
 local blackscreens = game:GetService("Players").LocalPlayer.PlayerGui.fadetoblack.frame
-local miscui = game:GetService("ReplicatedStorage").misc.otherMenu
 local sponsor = game:GetService("Players").LocalPlayer.PlayerGui.mainHUD.SponsorFrame
 
 --// stuff
 topbar.Visible = false
 blackscreens.Visible = false
-miscui.Visible = false
 sponsor.Visible = false
