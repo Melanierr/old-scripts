f814fb0c1b1b54e05ebb0e80d6f85154e3c8dc95
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-11-26 11:21:47 +0700

    Update Auto Finish.lua

--- decaying winter/Auto Finish.lua
+++ decaying winter/Auto Finish.lua
@@ -1,29 +1,23 @@
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.Interactables.NoDel.Touch, "touchthing", 30515) -- knife
+local stuff = game.Workspace.InteractablesNoDel
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(stuff.Touch, "touchthing", _G.serverKey) -- knife
 wait(0.3)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-74, 22, 166)) -- goto cultist small bunker wait
 wait(2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to spawn
 wait(2)
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Keycard, "keycardget", 30515)
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(stuff.Keycard, "keycardget", _G.serverKey)
 wait(2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to spawn
 wait(4)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(72, -3, -198)) -- go to the basement door
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Unlock, "unlockthing", 30515)
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(stuff.Unlock, "unlockthing", _G.serverKey)
 wait(3)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-14, -64, -250)) -- go to the small door
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-165, -77, -210)) -- go to the laptop room
 wait(0.2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(72, -3, -198)) -- go to the basement door
 wait(1)
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(workspace.InteractablesNoDel.Activate, "activatething", 30515)
+game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(stuff.Activate, "activatething", _G.serverKey)
 wait(1)
-local args = {
-    [1] = workspace.InteractablesNoDel:FindFirstChild("Acquire Masks"), -- get mask
-    [2] = "acquirething",
-    [3] = 30515
-}
-game:GetService("ReplicatedStorage").Interactables.interaction:FireServer(unpack(args))
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16)) -- go back to base
-wait(1)
-print("True ending is ready")
+e:GetService("ReplicatedStorage").Interactables.interaction:FireServer(stuff:FindFirstChild("Acquire Masks"), acquirething, _G.serverKey)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(400, -3, -16))
