4a381ff3cc100c1d9d56fc998e48582993c6aa1b
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-11-27 14:42:45 +0700

    Update Longer Effects.lua

--- decaying winter/Longer Effects.lua
+++ decaying winter/Longer Effects.lua
@@ -2,12 +2,12 @@ local eff = require(game:GetService("Workspace").ServerStuff.Statistics["S_STATI
 if eff then
     eff.RespA.dur = 120
     eff.BuffC.dur = 120
-    eff.Exha.dur = 0
-    eff.Crp.dur = 0
-    eff.Bld.dur = 0
-    eff.HeavBld.dur = 0
+    eff.Exha.dur = 1
+    eff.Crp.dur = 1
+    eff.Bld.dur = 1
+    eff.HeavBld.dur = 1
     eff.Stealth.dur = 400
-    eff.Vcd.dur = 0
+    eff.Vcd.dur = 1
     eff.Brz.dur = 240
     eff.Arm.dur = 1000
     eff.ArtiB.dur = 60
@@ -18,10 +18,10 @@ if eff then
     eff["S44-UL1"].dur = 360
     eff.Pnk.dur = 360
     eff.Laz.dur = 360
-    eff.Snr.dur = 0
-    eff.Tnt.dur = 0
+    eff.Snr.dur = 1
+    eff.Tnt.dur = 1
     eff.Slc.dur = 20
     eff.Rgn.dur = 360
-    eff.Bnd.dur = 0
+    eff.Bnd.dur = 1
     end
 
