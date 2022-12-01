f130c87a0f5f7869ce889549ae64195543eda20c
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-12-01 19:43:42 +0700

    Update Crate ESP.lua

--- decaying winter/Crate ESP.lua
+++ decaying winter/Crate ESP.lua
@@ -1,21 +1,21 @@
 for _,b in pairs(game.Workspace.Interactables:GetDescendants()) do
-if b.Parent.Name == "Crate"  then
-local BillboardGui = Instance.new("BillboardGui")
-local TextLabel = Instance.new("TextLabel")
+  if b.Parent.Name == "Crate"  then
+    local BillboardGui = Instance.new("BillboardGui")
+    local TextLabel = Instance.new("TextLabel")
 
-BillboardGui.Parent = b.Parent
-BillboardGui.AlwaysOnTop = true
-BillboardGui.LightInfluence = 1
-BillboardGui.Size = UDim2.new(0, 30, 0, 30)
-BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
+    BillboardGui.Parent = b.Parent
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.LightInfluence = 1
+    BillboardGui.Size = UDim2.new(0, 30, 0, 30)
+    BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
 
-TextLabel.Parent = BillboardGui
-TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-TextLabel.BackgroundTransparency = 1
-TextLabel.Size = UDim2.new(1, 0, 1, 0)
-TextLabel.Text = "crate"
-TextLabel.TextColor3 = Color3.new(1, 0, 0)
-TextLabel.TextScaled = true
-wait()
-end
+    TextLabel.Parent = BillboardGui
+    TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+    TextLabel.BackgroundTransparency = 1
+    TextLabel.Size = UDim2.new(1, 0, 1, 0)
+    TextLabel.Text = "crate"
+    TextLabel.TextColor3 = Color3.new(1, 0, 0)
+    TextLabel.TextScaled = true
+  wait()
+  end
 end
