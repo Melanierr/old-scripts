544e7e5c92ed20c6b0fb13cc7193fca181ff9169
Irish <97372099+IrishBaker@users.noreply.github.com>
2022-12-16 20:55:19 +0700

    Added team checking features.

--- Hitbox Expander.lua
+++ Hitbox Expander.lua
@@ -1,14 +1,25 @@
+_G.teamcheck = true -- team check
 local plr = game.Players.LocalPlayer
 while true do
     spawn(function()
+        if _G.teamcheck == true then
             for _,v in pairs(game.Players:GetPlayers()) do
                 if v ~= plr and v.Team ~= plr.Team then
                     v.Character.Head.Size = Vector3.new(4, 4, 4)
-                    v.Character.Head.Transparency = 0.2
+                    v.Character.Head.Transparency = 0.4
                     v.Character.Head.CanCollide = false
                 end
             end
-    wait()
+        end
+        if _G.teamcheck == false then
+            for _,v in pairs(game.Players:GetPlayers()) do
+                if v ~= plr then
+                    v.Character.Head.Size = Vector3.new(4, 4, 4)
+                    v.Character.Head.Transparency = 0.4
+                    v.Character.Head.CanCollide = false
+                end
+            end
+        end
     end)
 wait()
 end
