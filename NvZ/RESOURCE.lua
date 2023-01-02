109f056e74a99886ff7d37b641907d2b430f65b9
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-02 19:21:23 +0700

    Update RESOURCE.lua

--- NvZ/RESOURCE.lua
+++ NvZ/RESOURCE.lua
@@ -1,4 +1,4 @@
---// Instances
+--// Functions
 function gamepass()
     while true do
         for __,a in pairs(game.Players.LocalPlayer.ClassConfigurations.LocalGamepasses:GetChildren()) do
@@ -29,17 +29,17 @@ local InfiniteJumpEnabled = true
 local plr = game.Players.LocalPlayer.Character
 local cf = Vector3.new(1, 1, 1)
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Spawner", "Synapse")
-local Tab = Window:NewTab("Item")
+local Window = Library.CreateLib("NvZ:R", "Synapse")
+local Tab = Window:NewTab("Spawner")
 local Section = Tab:NewSection("DO NOT SPAM!!")
 --// Setups
-    Section:NewButton("Ammo Crate", "Nah", function()
+    Section:NewButton("Ammo Crate", "", function()
         game:GetService("ReplicatedStorage").Miscs.Events.AmmoBoxThrowEvent:FireServer(cf, game:GetService("Players").LocalPlayer.Character:FindFirstChild("Ammo Crate").Handle)
     end)
-    Section:NewButton("Explosive Crate", "Nah", function()
+    Section:NewButton("Explosive Crate", "", function()
         game:GetService("ReplicatedStorage").Miscs.Events.AmmoBoxThrowEvent:FireServer(cf, game:GetService("Players").LocalPlayer.Character:FindFirstChild("Explosive Crate").Handle)
     end)
-    Section:NewButton("Medic Box", "Nah", function()
+    Section:NewButton("Medic Box", "", function()
         game:GetService("ReplicatedStorage").Miscs.Events.MedicBoxThrowEvent:FireServer(cf, game:GetService("Players").LocalPlayer.Character:FindFirstChild("Medic Box").Handle, box)
     end)
 local Tabs = Window:NewTab("Misc")
@@ -56,6 +56,10 @@ local Section = Tabs:NewSection("WHAT")
     Section:NewButton("Unlocks Gamepasses", "Removes fog.", function()
         gamepass()
     end)
+    --[[
+    Section:NewButton("Use gas", "", function()
+    game:GetService("ReplicatedStorage").Miscs.Events.ActiveSupportEvent:FireServer(game.Players.LocalPlayer.Character:FindFirstChild("Gas Strike Radio").RadioScript, "Airstrike", game.Players.LocalPlayer.Character:FindFirstChild("Gas Strike Radio"), -5, 1406, -467)
+    end)]]
     Section:NewButton("Infinite Jump", "Allow you to jump mid-air.", function()
         game:GetService("UserInputService").JumpRequest:connect(function()
 	        if InfiniteJumpEnabled then
