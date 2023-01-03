17aa6ddd1030fae37f51aa278a27abb0d470aebe
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-03 22:11:17 +0700

    Update RESOURCE.lua

--- Anomalous Activites/RESOURCE.lua
+++ Anomalous Activites/RESOURCE.lua
@@ -16,46 +16,13 @@ function ESP()
     end)
 end
 local lobby = game:GetService("Workspace").lobby
-local mgame = game.Workspace.mainGame
 local InfiniteJumpEnabled = true
-local plr = game.Players.LocalPlayer
+local plr = game.Players.LocalPlayer.Character
 local cf = Vector3.new(1, 1, 1)
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Anomalous Activities", "Synapse")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("")
-    Section:NewButton("ESP", "Scary monster.", function()
-        ESP()
-        print("executed")
-    end)
-    Section:NewButton("Hitbox Extender", "Big head", function()
-        while true do
-            for _, box in pairs(mgame["active_anomaly"]:GetDescendants()) do
-                if box:IsA("Part") and box.Name == "Head" then
-                    box.Size =  Vector3.new(5, 5, 5)
-                    box.Transparency = 0.6
-                    box.CanCollide = false
-                end
-            end
-        wait()
-        end
-    end)
-    Section:NewButton("TeamKill", "IN DEV", function() 
-        for _, player in pairs(game.Players:GetPlayers()) do
-            if player ~= plr.Character and player.Character.Parent == mgame["active_humans"] then
-                player.Parent = mgame["active_anomaly"]
-            end
-        end
-    end)
-    Section:NewButton("Infinite Jump", "Scary monster.", function()
-        game:GetService("UserInputService").JumpRequest:connect(function()
-	        if InfiniteJumpEnabled then
-		        game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	        end
-        end)
-    end)
-local Tab = Window:NewTab("Misc")
-local Section = Tab:NewSection("Some features may make your game looks shitty")
     Section:NewButton("No Fog", "Removes fog.", function()
         while true do 
             game.Lighting.FogEnd = 1000000
@@ -71,6 +38,33 @@ local Section = Tab:NewSection("Some features may make your game looks shitty")
             wait()
         end
     end)
+    Section:NewButton("ESP", "Scary monster.", function()
+        ESP()
+        print("executed")
+    end)
+    Section:NewButton("Hitbox Extender", "Big head", function()
+        while true do
+            for _, box in pairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
+                if box:IsA("Part") and box.Name == "Head" then
+                    box.Size =  Vector3.new(5, 5, 5)
+                    box.Transparency = 0.6
+                    box.CanCollide = false
+                end
+            end
+        wait()
+        end
+    end)
+    Section:NewButton("Instant Anchor ( F key )", "", function()
+        game:GetService("UserInputService").InputBegan:Connect(function(Key)
+            if Key.KeyCode == Enum.KeyCode.F then -- put your custom key here
+                workspace.mainGame.remotes.game_handler:FireServer("interaction", {["with"]= workspace.CurrentMap.Interactables.A })
+                workspace.mainGame.remotes.game_handler:FireServer("interaction", {["with"]= workspace.CurrentMap.Interactables.B })
+                workspace.mainGame.remotes.game_handler:FireServer("interaction", {["with"]= workspace.CurrentMap.Interactables.C })
+                workspace.mainGame.remotes.game_handler:FireServer("interaction", {["with"]= workspace.CurrentMap.Interactables.D })
+                workspace.mainGame.remotes.game_handler:FireServer("interaction", {["with"]= workspace.CurrentMap.Interactables.E })
+            end
+        end)
+    end)
     Section:NewButton("Lag fix", "removes unnecesscary objects", function()
         for _, object in pairs(lobby:GetChildren()) do
             if object.Name == "lights" or object.Name == "corrodedbit" then
@@ -83,9 +77,7 @@ local Section = Tab:NewSection("Some features may make your game looks shitty")
                 material.Color = Color3.new(163, 162, 165)
             end
         end
-        game.Workspace.MedicalStuff:destroy()
-        game.Workspace.Posters:destroy()
     end)
-    Section:NewKeybind("Right Ctrl", "nigger", Enum.KeyCode.RightControl, function()
-	    Library:ToggleUI()
-    end)
+local alert = Instance.new("Sound",game:GetService("SoundService"))
+alert.SoundId = "rbxassetid://232127604"
+alert:Play()
