915a8027bd40b0ff5fc8b59e2000c6057e0bcfba
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-03 18:13:21 +0700

    Update Anomaly ESP.lua

--- Anomalous Activites/Anomaly ESP.lua
+++ Anomalous Activites/Anomaly ESP.lua
@@ -1,16 +1,11 @@
 while true do
     spawn(function()
-        for _, anomaly in pairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
-            if anomaly.Name == "HumanoidRootPart"  then
-                local h = Instance.new("Highlight") 
-                h.Parent = anomaly.Parent
-            end
-        end
-        for _,v in pairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
-            if v.Name == "Head" then
-                v.Size = Vector3.new(4, 4, 4)
-                v.Transparency = 0.6
-                v.CanCollide = false
+        for _, anomaly in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
+            if anomaly:IsA("Model")  then
+                local highlight = Instance.new("Highlight")
+                highlight.Parent = anomaly
+            else
+                warn("Not a player or a minion")
             end
         end
     end)
