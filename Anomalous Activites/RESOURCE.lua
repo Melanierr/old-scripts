06b611831426af0b6f9edcd34e360b53c2d41cb5
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-03 22:33:49 +0700

    Update RESOURCE.lua

--- Anomalous Activites/RESOURCE.lua
+++ Anomalous Activites/RESOURCE.lua
@@ -1,16 +1,10 @@
 function ESP()
     spawn(function()
         while true do
-            spawn(function()
                 for _, anomaly in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
-                    if anomaly:IsA("Model")  then
                         local highlight = Instance.new("Highlight")
                         highlight.Parent = anomaly
-                    else
-                        warn("Not a player or a minion")
-                    end
                 end
-            end)
         wait()
         end
     end)
