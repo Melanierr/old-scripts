83feccb4104d73e3f5fc8c6e468a97707b6f2787
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-05 18:40:20 +0700

    Update RESOURCE.lua

--- Anomalous Activites/RESOURCE.lua
+++ Anomalous Activites/RESOURCE.lua
@@ -4,7 +4,6 @@ until game:IsLoaded()
 wait(1)
 local lobby = game:GetService("Workspace").lobby
 local tk = false
-local InfiniteJumpEnabled = true
 local plr = game.Players.LocalPlayer.Character
 local alert = Instance.new("Sound",game:GetService("SoundService"))
 alert.SoundId = "rbxassetid://232127604"
@@ -42,16 +41,6 @@ local Section = Tab:NewSection("")
         wait()
         end
     end)
-    Section:NewButton("TeamKill", "OMG IMPOSTOR !!!", function()
-        while true do 
-            for _, player in pairs(game.Players:GetPlayers()) do
-                if player.Character.Parent == game.Workspace.mainGame["active_humans"] and player ~= game.Players.LocalPlayer then
-                    player.Character.Parent = game.Workspace.mainGame["active_firing_range"]
-                end
-            end
-        wait()
-        end
-    end)
     Section:NewToggle("TeamKill", "", function(state)
         if state then
             tk = true
@@ -64,7 +53,7 @@ local Section = Tab:NewSection("")
             wait()
             end
         else
-	    tk = false
+            tk = false
             while tk == false do
                 for _, player in pairs(game.Players:GetPlayers()) do
                     if player.Character.Parent == game.Workspace.mainGame["active_firing_range"] and player ~= game.Players.LocalPlayer then
@@ -89,15 +78,11 @@ local Section = Tab:NewSection("")
 local Tab = Window:NewTab("Misc")
 local Section = Tab:NewSection("")
     Section:NewButton("Fly Jump", "   T   ", function()
-        while true do
-            game.Players.LocalPlayer.Character.Humanoid.JumpPower = 20
-            wait()
-        end
         local InfiniteJumpEnabled = true
         game:GetService("UserInputService").JumpRequest:connect(function()
 	        if InfiniteJumpEnabled then
 		        game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	        end
+        	end
         end)
     end)
     Section:NewButton("No Fog", "Removes fog.", function()
@@ -117,12 +102,14 @@ local Section = Tab:NewSection("")
     Section:NewButton("Lag fix", "removes unnecesscary objects", function()
         game.Workspace.MedicalStuff:destroy()
         for _, effects in pairs(game:GetService("ReplicatedStorage")["misc_effects"]:GetChildren()) do
-            if effects:IsA("ParticleEmitter") or effects:IsA("Part") or effects:IsA("Part") then
+            if effects:IsA("ParticleEmitter") then
                 effects:destroy()
             end
         end
         for _,effect in pairs(game.ReplicatedStorage["weapon_effect"]:GetChildren()) do
-            effect:Destroy()
+            if effect:IsA("ParticleEmitter") then
+                effect:Destroy()
+            end
         end
         for _, object in pairs(lobby:GetChildren()) do
             if object.Name == "lights" or object.Name == "corrodedbit" then
