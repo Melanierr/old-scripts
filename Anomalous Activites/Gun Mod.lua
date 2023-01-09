5e20941ad27d248154bf7d666780cef049139ffa
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-09 23:11:45 +0700

    Update Gun Mod.lua

--- Anomalous Activites/Gun Mod.lua
+++ Anomalous Activites/Gun Mod.lua
@@ -13,3 +13,17 @@ for _, modules in pairs(game.ReplicatedStorage['weapon_modules']:GetChildren()) 
         ms.special_attributes = v31
     end
 end
+wait(1)
+for _, modules in pairs(game.ReplicatedStorage['weapon_modules']:GetChildren()) do
+        local ms = require(modules)
+        local v31 = {}
+        v31.full_acc_aimed = true
+        v31.trigger_delay = 0.1
+        v31.rpm_increase = 0.1
+        v31.rpm_max_increase = 20
+        v31.not_shown = false
+        v31.disallowed = false
+        ms.semi_automatic = false
+        ms.single_load = false
+        ms.special_attributes = v31
+end
