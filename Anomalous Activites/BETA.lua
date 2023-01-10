4717c660f2629f65beaf01b5a48dac6f868f4504
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-10 19:08:42 +0700

    Create BETA.lua

---
+++ Anomalous Activites/BETA.lua
@@ -0,0 +1,56 @@
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_variants.lmg_aa,
+        ["gui"] = workspace.merc_customisation.gui.primary
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_modules.sparemags,
+        ["gui"] = workspace.merc_customisation.gui.secondary
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_modules.sparemags,
+        ["gui"] = workspace.merc_customisation.gui.equipment1
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_modules.sparemags,
+        ["gui"] = workspace.merc_customisation.gui.equipment2
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
+
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_modules.sparemags,
+        ["gui"] = workspace.merc_customisation.gui.equipment3
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
+
+local args = {
+    [1] = "merc",
+    [2] = {
+        ["item"] = game:GetService("ReplicatedStorage").weapon_modules.sparemags,
+        ["gui"] = workspace.merc_customisation.gui.equipment4
+    }
+}
+
+workspace.mainGame.remotes.change_equipped:FireServer(unpack(args))
