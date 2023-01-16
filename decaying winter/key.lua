0beac56879561d3186affa2b7137562642c748a1
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-16 10:44:43 +0700

    Update key.lua

--- decaying winter/key.lua
+++ decaying winter/key.lua
@@ -1,17 +1,20 @@
-function getkey()
+function key()
     spawn(function()
-for _, instance in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-    if instance:IsA("LocalScript") and instance.Name ~= "ClickDetectorScript" then
-        repeat
-            mainHandler = getsenv(instance)
-            RunService.Heartbeat:Wait()
-        until mainHandler.afflictstatus ~= nil
-
-        local upvalue = getupvalues(mainHandler.afflictstatus)
-        _G.serverKey = upvalue[16]
-        _G.playerKey = upvalue[17]
-    end
+        while true do
+            for _, instance in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                if instance:IsA("LocalScript") and instance.Name ~= "ClickDetectorScript" then
+                    repeat
+                        mainHandler = getsenv(instance)
+                        game:GetService("RunService").Heartbeat:Wait()
+                    until mainHandler.afflictstatus ~= nil
+            
+                    local upvalue = getupvalues(mainHandler.afflictstatus)
+                    _G.serverKey = upvalue[16]
+                    _G.playerKey = upvalue[17]
+                end
+            end
+        wait(1)
+        end
+    end)
 end
-end)
-end
-getkey()
+key()
