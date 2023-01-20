792029209a34a91902d5c5219f72b1ec87f42969
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-01-20 09:35:15 +0700

    Update Longer Effects.lua

--- decaying winter/Longer Effects.lua
+++ decaying winter/Longer Effects.lua
@@ -1,27 +1,27 @@
 local eff = require(game:GetService("Workspace").ServerStuff.Statistics["S_STATISTICS"])
 if eff then
-    eff.RespA.dur = 120
-    eff.BuffC.dur = 120
+    eff.RespA.dur = math.huge
+    eff.BuffC.dur = math.huge
     eff.Exha.dur = 1
     eff.Crp.dur = 1
     eff.Bld.dur = 1
     eff.HeavBld.dur = 1
-    eff.Stealth.dur = 400
+    eff.Stealth.dur = math.huge
     eff.Vcd.dur = 1
     eff.Brz.dur = 240
-    eff.Arm.dur = 1000
+    eff.Arm.dur = math.huge
     eff.ArtiB.dur = 60
     eff.AlkyA.dur = 120
-    eff["3-(cbSTM)"].dur = 360
-    eff["I4S-DS"].dur = 360
-    eff["BL1 (Neloprephine)"].dur = 360
-    eff["S44-UL1"].dur = 360
-    eff.Pnk.dur = 360
-    eff.Laz.dur = 360
+    eff["3-(cbSTM)"].dur = math.huge
+    eff["I4S-DS"].dur = math.huge
+    eff["BL1 (Neloprephine)"].dur = math.huge
+    eff["S44-UL1"].dur = math.huge
+    eff.Pnk.dur = math.huge
+    eff.Laz.dur = math.huge
     eff.Snr.dur = 1
     eff.Tnt.dur = 1
     eff.Slc.dur = 20
-    eff.Rgn.dur = 360
+    eff.Rgn.dur = math.huge
     eff.Bnd.dur = 1
-    end
+end
 
