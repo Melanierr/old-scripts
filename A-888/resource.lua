14ca5d05e6a826137d6c2d3ca90e31ed342923c2
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-02-10 17:49:51 +0700

    Update resource.lua

--- A-888/resource.lua
+++ A-888/resource.lua
@@ -2,9 +2,10 @@ if game.PlaceId == 12389327869 then
     if game:IsLoaded() then
         local plr = game.Players.LocalPlayer
         local rs =game:GetService("ReplicatedStorage")
-        local cgui = workspace.merc_customisation.gui.primary
-        local mo =game:GetService("ReplicatedStorage").weapon_modules
+        local mo = game:GetService("ReplicatedStorage").weapon_modules
         local anomaly = game:GetService("Workspace").mainGame.active_anomaly
+       plr.PlayerGui.mainGui["death_screen"].Visible=false
+        plr.PlayerGui.mainGui.splatterOverlay.Visible=false
         function hb()
         end
         function rm()
@@ -15,12 +16,72 @@ if game.PlaceId == 12389327869 then
             game:GetService("Workspace").jfj:Destroy()
             game:GetService("Workspace").Posters:Destroy()
         end
-        local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-        local Window = Library.CreateLib("KFC Hub", "Synapse")
-        Tab = Window:NewTab("Main")
-        local Section = Tab:NewSection("")
-        Section:NewButton("OneShot", "You use 50 Cal. bullet.", function()
-            local settings = {repeatamount = 10, exceptions = {"SayMessageRequest", "game_handler", "audio_relay", "weapon_vfx_relay", "voices_handler", "arm_handler", "player_respawn", "change_equipped", "resetter", "standing_handler", "effects_relay", "weapon_handler", "terror_handler"}}
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("KFC Hub", "Synapse")
+    Tab = Window:NewTab("Main")
+    local Section = Tab:NewSection("")
+        Section:NewButton("Fly Jump", "", function()
+            local InfiniteJumpEnabled = true
+            game:GetService("UserInputService").JumpRequest:connect(function()
+                plr.Character.Humanoid.JumpPower = 20
+            	if InfiniteJumpEnabled then
+            		plr.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+            	end
+            end)
+        end)
+        Section:NewButton("Anti Lag", "", function()
+            rm()
+            game.Lighting.FogEnd=100000
+            for _,lag in pairs(game:GetDescendants()) do
+                if lag:IsA("Particle Emitter") or lag:IsA("Beam") then
+                    lag.Enabled = false
+                end
+                if lag:IsA("PointLight") then
+                    lag:Destroy()
+                    wait(.1)
+                end
+                if lag:IsA("Part") then
+                    lag.Material = "Plastic"
+                    lag.Color=Color3.new(112, 113, 121)
+                    wait(.1)
+                end
+                if lag:IsA("Texture") then
+                    lag:Destroy()
+                    wait(.1)
+                end
+            end
+        end)
+    Tab = Window:NewTab("Weapon")
+    local Section = Tab:NewSection("")
+        Section:NewButton("Show hidden slot & guns", "", function()
+            game:GetService("Workspace")["merc_customisation"].gui.equipmentip3:Destroy()
+            game:GetService("Workspace")["merc_customisation"].gui.equipmentip3:Destroy()
+            game:GetService("Workspace")["merc_customisation"].gui.equipment4.Visible = true
+            for _,a in pairs(game.ReplicatedStorage.weapon_modules:GetChildren()) do
+                local b = require(a)
+                b.special_attributes = {not_shown = false,disallowed = false}
+            end
+        end)
+        Section:NewButton("Inf Mags", "", function()
+            for _,a in pairs(rs.weapon_modules:GetChildren()) do
+                if a.Name ~= "sentry" or a.Name ~='pipe' or a.Name ~= 'camera' or a.Name~='smoke' or a.Name ~= "syringe" or a.Name ~='mine' or a.Name ~= 'platepiece' or a.Name~='plate' or a.Name ~= "medic" or a.Name ~='stamshot' or a.Name ~= 'shrap' or a.Name~='ammobox' or a.Name ~= 'throwaxe' then
+                    local b = require(a)
+                    b.special_attributes = {spare_modifier=100}
+                    print("blaclisted")
+                    wait(.01)
+                end
+            end
+        end)
+        Section:NewButton("Full Auto", "", function()
+            for _,a in pairs(rs.weapon_modules:GetChildren()) do
+                if a.Name ~= "sentry" or a.Name ~='pipe' or a.Name ~= 'camera' or a.Name~='smoke' or a.Name ~= "syringe" or a.Name ~='mine' or a.Name ~= 'platepiece' or a.Name~='plate' or a.Name ~= "medic" or a.Name ~='stamshot' or a.Name ~= 'shrap' or a.Name~='ammobox' or a.Name ~= 'throwaxe' then
+                local b = require(a)
+                b.semi_automatic = false
+                end
+            end
+        end)
+        Section:NewButton("OneShot", "", function()
+            local settings = {repeatamount = 15, exceptions = {"SayMessageRequest", "game_handler", "audio_relay", "weapon_vfx_relay", "voices_handler", "arm_handler", "player_respawn", "change_equipped", "resetter", "standing_handler", "effects_relay", "weapon_handler", "terror_handler", "equipment_handler", 'aux_handler', 'skill_handler'}}
             local mt = getrawmetatable(game)
             local old = mt.__namecall
             setreadonly(mt, false)
@@ -42,57 +103,28 @@ if game.PlaceId == 12389327869 then
             end
             setreadonly(mt, true)
         end)
-        Section:NewButton("Fly Jump", "", function()
-            local InfiniteJumpEnabled = true
-            game:GetService("UserInputService").JumpRequest:connect(function()
-                plr.Character.Humanoid.JumpPower = 20
-            	if InfiniteJumpEnabled then
-            		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-            	end
-            end)
-        end)
-        Section:NewButton("Anti Lag", "", function()
-            rm()
-            for _,lag in pairs(rs:GetDescendants()) do
-                if lag:IsA("Particle Emitter") or lag:IsA("Beam") then
-                    lag.Enabled = false
-                end
-                if lag:IsA("PointLight") then
-                    lag:Destroy()
-                end
-                wait(.1)
+    Tab = Window:NewTab("Misc")
+    local Section = Tab:NewSection("")
+        Section:NewDropdown("Equip item", "", {"GT-HBR", "R.300", "M9P", "M9-S", "", ""}, function(currentOption)
+            if currentOption=="GT-HBR" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.sniperblack,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.primary})
+            end
+            if currentOption=="R.300" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.blackgun,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.primary})
+            end
+            if currentOption=="M9P" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.akimboblack,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.pistol})
+            end
+            if currentOption=="M9-S" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.suppistol,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.pistol})
+            end
+            if currentOption=="M9P" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.akimboblack,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.pistol})
+            end
+            if currentOption=="M9P" then
+                workspace.mainGame.remotes.change_equipped:FireServer("merc", {["item"] = rs.weapon_modules.akimboblack,  ["gui"] = game:GetService("Workspace").merc_customisation.gui.pistol})
             end
         end)
-        Section:NewButton("Show hidden equipment slot", "", function()
-            game:GetService("Workspace")["merc_customisation"].gui.equipmentip3:Destroy()
-            game:GetService("Workspace")["merc_customisation"].gui.equipmentip3:Destroy()
-            game:GetService("Workspace")["merc_customisation"].gui.equipment4.Visible = true 
-        end)
-        Section:NewButton("Increase monster's money [ NOT WORKING ]", "", function()
-            for _, enemy in pairs(rs.enemy_types:GetChildren()) do
-                local script = require(enemy)
-                script.v1.kill_value = 500
-            end
-        end)
-        Tab = Window:NewTab("Misc")
-        local Section = Tab:NewSection("")
-        Section:NewDropdown("Special Stuff [ Some option may not work ]", "", {"GT-HBR", "R.300", "M9P+", "BSD set"}, function(currentOption)
-            if currentOption == "GT-HBR" then
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",cgui, mo.sniperblack)
-            end
-            if currentOption == "R.300" then
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",cgui, mo.blackgun)
-            end
-            if currentOption == "M9P+" then
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",workspace.merc_customisation.gui.pistol, akimboblack)
-            end
-            if currentOption == "BSD set" then
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",workspace.merc_customisation.gui.pistol, akimboblack)
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",workspace.merc_customisation.gui.secondary, mo.blackgun)
-                workspace.mainGame.remotes.change_equipped:FireServer("merc",cgui, mo.sniperblack)
-            end
-        end)
-
         Section:NewKeybind("Right Ctrl to close", "", Enum.KeyCode.RightControl, function()
         	Library:ToggleUI()
         end)
