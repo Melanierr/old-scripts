b271f6dd0899c6f5b5119bb7e467e155410faaf7
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-10-21 08:38:31 +0700

    Update ui.lua

--- HELMET/ui.lua
+++ HELMET/ui.lua
@@ -1,7 +1,7 @@
 function check()
 	for _,a in pairs (workspace:GetChildren()) do
-		if a.Name =="Hostile" then
-			local b =Instance.new("Highlight", a)
+		if a.Name == "Hostile" and a.Name ~= players.LocalPlayer and a.Name ~= "Civilian" then
+			local b = Instance.new("Highlight", a)
 		end
 	end
 end
