1f89f37bf93a89e2764006e6765098abd767371a
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-10-21 08:33:27 +0700

    Update ui.lua

--- HELMET/ui.lua
+++ HELMET/ui.lua
@@ -1,17 +1,11 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("HELMET", "Synapse")
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("More will be added!")
-Section:NewButton("Hostile ESP", "", function()
-	function check()
-		for _,a in pairs (workspace:GetChildren()) do
-			if a.Name =="Hostile" then
-				local b =Instance.new("Highlight", a)
-			end
+function check()
+	for _,a in pairs (workspace:GetChildren()) do
+		if a.Name =="Hostile" then
+			local b =Instance.new("Highlight", a)
 		end
 	end
-    game.Workspace.ChildAdded:Connect(function()
-		check()
-	end)	
+end
+game.Workspace.ChildAdded:Connect(function()
 	check()
-end)
+end)	
+check()
