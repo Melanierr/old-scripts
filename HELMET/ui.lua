5ace61f4ea18d4b037d8b95e2fd6b2c3167f871d
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-10-21 08:48:06 +0700

    Update ui.lua

--- HELMET/ui.lua
+++ HELMET/ui.lua
@@ -1,3 +1,9 @@
+local objectives = workspace.Objectives
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("HELMET", "Synapse")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("More will be added!")
+Section:NewButton("Hostile ESP", "", function()
 function check()
 	for _,a in pairs (workspace:GetChildren()) do
 		if a.Name == "Hostile" and a.Name ~= players.LocalPlayer and a.Name ~= "Civilian" then
@@ -5,7 +11,23 @@ function check()
 		end
 	end
 end
-game.Workspace.ChildAdded:Connect(function()
+	game.Workspace.ChildAdded:Connect(function()
+		wait(2)
+		check()
+	end)	
 	check()
-end)	
-check()
+end)
+Section:NewButton("Skip Keycard Door", "", function()
+	game:GetService("Workspace").Map.Objectives:FindFirstChild("KeycardDoor"):Destroy()
+end)
+Section:NewButton("Check for Keycard", "", function()
+	local kc = Instance.new("Highlight", game:GetService("Workspace").Map.Geometry.CameraRoom.KeycardSpawns:FindFirstChild("Keycard")
+end)
+Section:NewButton("Objective ESP", "", function()
+	for _,o in pairs(objectives:GetChildren()) do
+		local opj = Instance.new("Highlight", o)
+	end
+	objectives.ChildAdded:Connect(function(ob)
+		local obj = Instance.new("Highlight", ob)
+	end)
+end)
